66bf8dd09c3baa016d0a28e534ec763dbf909984
Ulysses ALvarez <ulyssesbytes@gmail.com>
2018-02-16 12:09:57 +0000

    adding extra files

---
+++ app/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Route, Router, IndexRoute, hashHistory} from 'react-router';
+import TodoApp from 'TodoApp';
+
+// // Load foundation
+// $(document).foundation();
+// // App css
+// require('style!css!sass!applicationStyles')
+ReactDOM.render(
+  <TodoApp/>,
+  document.getElementById('app')
+);
